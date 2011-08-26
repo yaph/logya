2be4005d81e1915e919494edcb26bb5e6d21ff20
Ramiro Gomez <web@ramiro.org>
2011-08-27 01:48:31 +0200

    added custom stylesheet with Bootstrap overrides; copy static dir to target site

--- geeklog/geeklog.py
+++ geeklog/geeklog.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import glob
+import shutil
 
 cwd = os.getcwd()
 sys.path.append(os.path.join(cwd, 'lib'))
@@ -16,3 +17,9 @@ posts = glob.glob(os.path.join(cwd, 'example_site', 'posts', '*.html'))
 for p in posts:
     doc = DocWriter(p, template_env)
     doc.write()
+
+# copy static files
+src = os.path.join(cwd, 'example_site', 'static')
+dst = os.path.join(cwd, 'site', 'static')
+shutil.rmtree(dst, True)
+shutil.copytree(src, dst)
