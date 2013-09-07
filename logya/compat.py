75005cf50b7724d0747060e902b97d30b010d4f1
Ramiro Gómez <code@ramiro.org>
2013-09-08 01:07:20 +0200

    work on python3 compatibility

---
+++ logya/compat.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+logya.compat
+~~~~~~~~~~~~~
+
+Imports and declarations for Python 2 and Python 3 compatibility.
+Based on
+https://github.com/michaelhelmick/lassie/blob/master/lassie/compat.py
+"""
+
+import sys
+
+_ver = sys.version_info
+
+
+if (_ver[0] == 2):
+    import ConfigParser as configparser
+    from urllib import quote_plus
+    from urlparse import urlparse
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
+
+    str = unicode
+
+elif (_ver[0] == 3):
+    import configparser
+    from urllib.parse import quote_plus
+    from urlparse.parse import urlparse
+    from http.server import SimpleHTTPRequestHandler
+    from http.server import HTTPServer
+
+    str = str
