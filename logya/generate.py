6c5a3e68ed8ce9cb9e321487621a01d055568159
Ramiro Gómez <code@ramiro.org>
2014-11-20 19:07:15 +0100

    code style

--- logya/generate.py
+++ logya/generate.py
@@ -14,7 +14,7 @@ class Generate(Logya):
         super(self.__class__, self).__init__(**kwargs)
         self.init_env()
 
-        # init writer before executing scripts, so they can use it
+        # Init writer before executing scripts, so they can use it.
         self.writer = DocWriter(self.dir_dst, self.template)
 
         self.info("Remove existing deploy directory")
@@ -26,16 +26,13 @@ class Generate(Logya):
             self.info("Copy static files")
             shutil.copytree(self.dir_static, self.dir_dst)
 
-        # execute scripts in before building indexes, so that generated content
-        # can be indexed too
+        # Execute scripts in before building indexes, so that generated content
+        # can be indexed too.
         self.info("Execute scripts in bin dir")
         self.exec_bin()
 
         self.info("Build document indexes")
         self.build_indexes()
-
-        #template_env = self.template.get_env()
-        #template_env.globals['__docs__'] = self.docs_parsed
 
         self.info("Write documents to deploy directory")
         for doc in list(self.docs_parsed.values()):
