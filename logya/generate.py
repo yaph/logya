8beb6ddd2e58d6a3e54ab297d490c6650fb85a9d
Ramiro Gómez <code@ramiro.org>
2019-01-23 22:17:39 +0100

    Add build and write function to make it easy to subclass Generate and overwrite build step

--- logya/generate.py
+++ logya/generate.py
@@ -11,26 +11,27 @@ class Generate(Logya):
     """Generate a Web site to deploy from current directory as source."""
 
     def __init__(self, **kwargs):
-
         super(self.__class__, self).__init__(**kwargs)
         self.init_env()
-
-        # Init writer before executing scripts, so they can use it.
         self.writer = DocWriter(self.dir_deploy, self.template)
 
         if not kwargs['keep']:
             self.info('Remove existing deploy directory')
             shutil.rmtree(self.dir_deploy, True)
 
-        self.info('Generating site in directory: {}'.format(self.dir_deploy))
-
+        self.info('Generate site in directory: {}'.format(self.dir_deploy))
         if os.path.exists(self.dir_static):
             self.info('Copy static files')
             copytree(self.dir_static, self.dir_deploy)
 
+        self.build()
+        self.write()
+
+    def build(self):
         self.info('Build document index')
         self.build_index()
 
+    def write(self):
         self.info('Write documents')
         for doc in self.docs.values():
             self.writer.write(doc, self.get_doc_template(doc))
