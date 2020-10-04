020f7b48a1375f3b8b1d966dab12e6db15128ce2
Ramiro Gómez <code@ramiro.org>
2020-10-05 01:25:41 +0200

    Add class variable L to HTTPRequestHandler to access Logya instance inside methods.

--- logya/server.py
+++ logya/server.py
@@ -7,82 +7,84 @@ from shutil import copyfile
 from urllib.parse import unquote, urlparse
 
 from logya.core import Logya
-from logya.content import add_collections, read, read_all, write_collection, write_page
-from logya.util import load_settings
-
-
-settings = load_settings()
-site_index = read_all(settings)
-
-L = Logya()
-L.init_env()
-L.build_index()
-L.template.vars['debug'] = True
+from logya.content import add_collections, read, write_collection, write_page
 
 
 class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     """SimpleHTTPRequestHandler based class to return resources."""
 
+    L = None
+
     def __init__(self, *args):
-        super(HTTPRequestHandler, self).__init__(*args, directory=settings['paths']['public'].as_posix())
+        super(HTTPRequestHandler, self).__init__(*args, directory=self.L.paths.public.as_posix())
 
     def do_GET(self):
-        update_resource(self.path)
+        update_resource(self.path, self.L)
         super(HTTPRequestHandler, self).do_GET()
 
 
-def update_resource(path):
+def update_resource(path, L):
     """Update resource corresponding to given url.
 
     Resources that exist in the `static` directory are updated if they are newer than the destination file.
-    For other HTML resources the whole `site_index` is updated and the destination is newly written."""
+    For other HTML resources the whole `L.index` is updated and the destination is newly written."""
 
     # Use only the actual path and ignore possible query params (see issue #3).
     path = unquote(urlparse(path).path)
     path_rel = path.lstrip('/')
 
     # If a static file is requested update it and return.
-    src_static = Path(settings['paths']['static'], path_rel)
+    src_static = Path(L.paths.static, path_rel)
     if src_static.is_file():
-        dst_static = Path(settings['paths']['public'], path_rel)
+        dst_static = Path(L.paths.public, path_rel)
         dst_static.parent.mkdir(exist_ok=True)
         if not dst_static.exists() or src_static.stat().st_mtime > dst_static.stat().st_mtime:
-            print(f'Update static resource: {dst_static}')
+            L.info(f'Update static resource: {dst_static}')
             copyfile(src_static, dst_static)
         return
 
     # Rebuild index for HTML file requests which are not in index.
-    if path.endswith(('/', '.html', '.htm')) and path not in site_index:
-        print(f'Rebuild index for request URL: {path}')
-        site_index.update(read_all(settings))
+    if path.endswith(('/', '.html', '.htm')) and path not in L.index:
+        L.info(f'Rebuild index for request URL: {path}')
+        L.build_index()
 
     # Requested path does not exist.
-    if path not in site_index:
+    if path not in L.index:
         return
 
-    content = site_index[path]
-    path_dst = Path(settings['paths']['public'], path_rel, 'index.html')
+    content = L.index[path]
+    path_dst = Path(L.paths.public, path_rel, 'index.html')
 
     # Update content document.
     if 'doc' in content:
-        content['doc'] = read(content['path'], settings)
-        if 'collections' in settings:
-            add_collections(content['doc'], site_index, settings['collections'])
+        content['doc'] = read(content['path'], L.settings)
+        if 'collections' in L.settings:
+            add_collections(content['doc'], L.index, L.settings['collections'])
         # Always write doc because of possible template changes.
-        write_page(path_dst, content, L.template, settings)
-        print(f'Refreshed doc at URL: {path}')
+        write_page(path_dst, content, L.settings)
+        L.info(f'Refreshed doc at URL: {path}')
 
     # Update collection page.
     if 'docs' in content:
-        write_collection(path_dst, content, L.template, settings)
-        print(f'Refreshed collection: {path}')
+        write_collection(path_dst, content, L.settings)
+        L.info(f'Refreshed collection: {path}')
 
 
-def serve(args):
+def serve(options):
+    base_url = f'http://{options.host}:{options.port}'
+
     # FIXME L.template.vars not used in write_page
-    L.template.vars['base_url'] = f'http://{args.host}:{args.port}'
+    # L.template.vars['base_url'] = base_url
+    # L.template.vars['debug'] = True
+
+    L = Logya(options)
+    # L.init_env()
+    # L.build_index()
+
+    HTTPRequestHandler.L = L
+
     # Avoid "OSError: [Errno 98] Address already in use"
     socketserver.TCPServer.allow_reuse_address = True
-    with socketserver.TCPServer((args.host, args.port), HTTPRequestHandler) as httpd:
-        print(f'Serving on {L.template.vars["base_url"]}')
+    with socketserver.TCPServer((options.host, options.port), HTTPRequestHandler) as httpd:
+        print(f'Serving on {base_url}')
         httpd.serve_forever()
