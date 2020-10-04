f0cc0afb84dee4c81c08b8c454740f0fff88b917
Ramiro Gómez <code@ramiro.org>
2020-10-05 01:23:30 +0200

    Load settings and paths in util.py

--- logya/util.py
+++ logya/util.py
@@ -1,32 +1,56 @@
 # -*- coding: utf-8 -*-
 import re
 
-import yaml
-
+from collections import namedtuple
 from pathlib import Path
+
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 
 # Characters not to be used in URLs
 re_forbidden = re.compile(r'[\s\/\?\+\{\}\|\\\[\]:;&$=]+')
 
-
-def load_settings():
-    """Return a dictionary with global settings."""
-
-    root = Path.cwd()
-    settings = {
-        'paths': {
-            'root': root,
-            'content': Path(root, 'content'),
-            'templates': Path(root, 'templates'),
-            'static': Path(root, 'static'),
-            'public': Path(root, 'public')
-        }
-    }
-    settings.update(yaml.load(Path(root, 'site.yaml').read_text(), Loader=yaml.FullLoader))
-    return settings
+directories = ['root', 'content', 'public', 'static', 'templates']
+Paths = namedtuple('Paths', directories)
 
 
+# FIXME use tests in test_canonical_filename
+def filepath(base: Path, url: str) -> Path:
+    """Get a Path object pointing to a file.
+
+    If url does not end in a file name 'index.html' will be appended.
+    """
+
+    path = Path(base, url.lstrip('/'))
+    if not path.suffix:
+        path = path.joinpath('index.html')
+    return path
+
+
+def paths(dir_site: str = None) -> dict:
+    if dir_site:
+        root = Path(dir_site)
+    else:
+        root = Path.cwd()
+
+    return Paths(
+        root=root,
+        content=root.joinpath('content'),
+        templates=root.joinpath('templates'),
+        static=root.joinpath('static'),
+        public=root.joinpath('public')
+    )
+
+
+def load_yaml(text: str) -> dict:
+    return load(text, Loader=Loader)
+
+
+# FIXME use tests in test_slugify
 def slugify(s: str) -> str:
     """Return string with forbidden characters replaced with hyphens.
 
---
+++ tests/test_util.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from logya.core import Logya
+
+L = Logya(dir_site='logya/sites/bare')
