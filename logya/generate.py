44233af1e6cdc368a866c4a96ee4b1dfa53cc870
Ramiro Gómez <code@ramiro.org>
2020-10-05 20:36:22 +0200

    Call build_index and use joinpath.

--- logya/generate.py
+++ logya/generate.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import shutil
 
-from pathlib import Path
 from shutil import copytree
 
 from logya.core import Logya
@@ -10,7 +9,7 @@ from logya.content import write_collection, write_page
 
 def generate(options):
     L = Logya(options)
-    #L.build_index()
+    L.build_index()
 
     if not options.keep:
         print('Remove existing public directory.')
@@ -23,7 +22,7 @@ def generate(options):
 
     print('Write documents.')
     for url, content in L.index.items():
-        L.paths.dst = Path(L.paths.public, url.lstrip('/'), 'index.html')
+        L.paths.dst = L.paths.public.joinpath(url.lstrip('/'), 'index.html')
         if 'doc' in content:
             print(f'Write document page to: {L.paths.dst}')
             write_page(L.paths.dst, content, L.settings)
