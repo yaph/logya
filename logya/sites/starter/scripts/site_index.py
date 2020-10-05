76a821ff63da3fd55920557e89a3784e102957d5
Ramiro Gómez <code@ramiro.org>
2020-10-06 01:19:36 +0200

    Port to v5

--- logya/sites/starter/scripts/site_index.py
+++ logya/sites/starter/scripts/site_index.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 # Run this script from your Logya site directory, e. g.:
 # python scripts/site_index.py
-import io
-import os
 import json
 
 from logya.core import Logya
 from logya.encoder import JSONEncoder
 
 
-logya = Logya()
-logya.init_env()
-logya.build_index()
-
+L = Logya()
+L.build()
 site_index = {}
-for url, doc in logya.docs.items():
+
+for url, doc in L.index.items():
     del doc['body']
     site_index[url] = doc
 
-index_file = os.path.join(logya.dir_static, 'site_index.json')
-with io.open(index_file, 'w', encoding='utf-8') as f:
-    json.dump(site_index, f, cls=JSONEncoder)
+index_file = L.paths.static.joinpath('site_index.json')
+index_file.write_text(json.dumps(site_index, cls=JSONEncoder))
