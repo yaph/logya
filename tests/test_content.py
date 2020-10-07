a185f4e3602de3762b226f07133451c461699739
Ramiro Gómez <code@ramiro.org>
2020-10-08 00:32:08 +0200

    Add test_create_url

--- tests/test_content.py
+++ tests/test_content.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from logya.content import read
+import logya.content
+
+from pathlib import Path
+
+from logya.util import paths
+
+
+site_root = 'tests/fixtures/site/'
+site_paths = paths(site_root)
+
+
+def test_create_url():
+    for value, expected in [
+        ('index.md', '/'),
+        ('sitemap.xml', '/sitemap.xml'),
+        ('path/to/index.md', '/path/to/'),
+        ('path/to/name.md', '/path/to/name/')]:
+
+        assert logya.content.create_url(Path(value)) == expected
+
 
 
 def test_read_markdown():
-    doc = read('tests/fixtures/site/content/markdown.md')
+    doc = logya.content.read(Path(site_root, 'content', 'markdown.md'), site_paths)
     assert isinstance(doc, dict)
     assert '/test/markdown/' == doc['url']
