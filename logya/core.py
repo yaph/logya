2f798eef406baafa8c714a212e50c0b0d9a0b88e
Ramiro Gómez <code@ramiro.org>
2020-10-09 02:52:28 +0200

    Add read_content and update_collections methods to Logya class.

--- logya/core.py
+++ logya/core.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-from logya.content import read_all
+from os import walk
+from pathlib import Path
+
+from logya.content import read
 from logya.template import init_env
-from logya.util import load_yaml, paths
+from logya.util import load_yaml, paths, process_extensions, slugify
 
 
 class Logya:
@@ -14,21 +17,70 @@ class Logya:
         self.paths = paths(dir_site=getattr(options, 'dir_site', None))
         self.settings = load_yaml(self.paths.root.joinpath('site.yaml').read_text())
 
-        # Initialize index so scripts can generate indexed content before build.
+        # Initialize index and collections so scripts can generate indexed content before build.
         self.index = {}
+        self.collections = self.settings.get('collections', {})
+        for name, coll in self.collections.items():
+            coll['index'] = {}
 
     def build(self):
-        """Build index of documents and collections."""
+        """Read content and initialize template environment."""
 
-        # Previously indexed content can exist. If a document exists in the read directory with the same URL as an
-        # already indexed content object, the existing object will be overridden.
-        self.index.update(read_all(self.paths, self.settings))
+        self.read_content()
 
         # Initialize template env.
         init_env(self.settings, self.index)
 
-    def info(self, msg):
+    def info(self, msg: str):
         """Print message if in verbose mode."""
 
         if self.verbose:
             print(msg)
+
+    def read_content(self):
+        """Read content and update index and collections.
+
+        Previously indexed content can exist. If a file inside the content directory has the same URL as already indexed
+        content, the existing content will be replaced.
+        """
+
+        for root, _, files in walk(self.paths.content):
+            for f in files:
+                path = Path(root, f)
+                if path not in process_extensions:
+                    continue
+                doc = read(path, path.relative_to(self.paths.content))
+                if doc:
+                    if self.collections:
+                        self.update_collections(doc)
+                    self.index[doc['url']] = {'doc': doc, 'path': path}
+
+    def update_collections(self, doc: dict):
+        """Update collections index for given doc."""
+
+        for attr, values in doc.items():
+            if attr not in self.collections:
+                continue
+            coll = self.collections[attr]
+
+            # Process unique values.
+            for value in set(values):
+                url = f'/{coll["path"]}/{slugify(value).lower()}/'
+
+                if url in self.index:
+                    print(f'Collection not created because content exists at {url}.')
+                    continue
+
+                # Add attribute for creating collection links in templates.
+                links = attr + '_links'
+                doc[links] = doc.get(links, []) + [(url, value)]
+
+                # Update or create collection index value.
+                if url in coll['index']:
+                    coll['index'][url]['docs'].append(doc)
+                else:
+                    coll['index'][url] = {
+                        'docs': [doc],
+                        'title': value,
+                        'template': coll['template']
+                    }
