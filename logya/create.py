cc491cb106b6e91ffb75a936fe07b3d5366ff2e5
Ramiro Gómez <code@ramiro.org>
2020-10-28 22:05:44 +0100

    Call sys.exit on error in create mode.

--- logya/create.py
+++ logya/create.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import shutil
 
+from sys import exit
+
 from pkg_resources import resource_filename
 
 from logya.util import paths
@@ -9,12 +11,11 @@ from logya.util import paths
 def create(dir_site: str, name: str, site: str = None, **kwargs):
     target = paths(dir_site=dir_site).root.joinpath(name)
     if target.exists():
-        print(f'Error: "{target}" already exists. Please remove it or specify another location.')
-        return
+        exit(f'Error: "{target}" already exists. Please remove it or specify another location.')
     try:
         source = resource_filename(__name__, f'sites/{site}')
     except KeyError:
-        print(f'The site "{site}" is not installed.')
+        exit(f'The site "{site}" is not installed.')
     else:
         shutil.copytree(source, target)
         print(f'Site created in "{target}".')
