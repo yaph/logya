fe93603e0897c2c95b7c0553b684186c30f26b6e
Ramiro Gómez <code@ramiro.org>
2020-11-19 00:26:13 +0100

    Fix merge conflicts Remove sphinx docs

--- logya/__init__.py
+++ logya/__init__.py
@@ -1,21 +1,4 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Ramiro Gómez'
 __email__ = 'code@ramiro.org'
-<<<<<<< HEAD
-__version__ = '4.7.2'
-
-# Extensions of content files that will be processed by Logya.
-allowed_exts = {
-    'html',
-    'htm',
-    'xml',
-    'json',
-    'js',
-    'css',
-    'php',
-    'md',
-    'markdown',
-    'txt'}
-=======
 __version__ = '5.0.0'
->>>>>>> v5
