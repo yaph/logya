05dcd2171f6508aeefc181f4e87f660f24db3b5b
Ramiro Gómez <code@ramiro.org>
2024-11-05 22:31:28 +0100

    Replace pkg_resources with importlib

--- logya/create.py
+++ logya/create.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 import shutil
+import sys
 
-from sys import exit
-
-from pkg_resources import resource_filename
+from importlib import resources
 
 from logya.util import paths
 
 
-def create(dir_site: str, name: str, site: str = None, **kwargs):
+def create(dir_site: str, name: str, site: str | None = None, **kwargs):
     target = paths(dir_site=dir_site).root.joinpath(name)
     if target.exists():
-        exit(f'Error: "{target}" already exists. Please remove it or specify another location.')
+        sys.exit(f'Error: "{target}" already exists. Please remove it or specify another location.')
     try:
-        source = resource_filename(__name__, f'sites/{site}')
+        source = resources.files(__name__).joinpath(f'sites/{site}')
     except KeyError:
-        exit(f'The site "{site}" is not installed.')
+        sys.exit(f'The site "{site}" is not installed.')
     else:
         shutil.copytree(source, target)
         print(f'Site created in "{target}".')
